4138be0758c22447bfedaf992e43e91193bf4d0a
PedroHamzehT <pedrohamzeh@gmail.com>
2024-02-02 23:05:43 -0300

    chore: adding real-time update with turbo stream broadcast

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,10 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+
+  # after_create_commit { broadcast_append_to 'quotes', target: 'quotes', partial: 'quotes/quote', locals: { quote: self } }
+  # after_update_commit { broadcast_replace_to 'quotes', target: self, partial: 'quotes/quote', locals: { quote: self } }
+  # after_destroy_commit { broadcast_remove_to 'quotes', target: self }
+
+  # All the code above can be replaced by the following line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
